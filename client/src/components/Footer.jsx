c7ccc6640e5db986cf2c056c9463d3ce6795a789
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-10 14:44:51 -0600

    set up stage 1

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return(
+        <footer>
+            <p>© 2023 - Todos os direitos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App.jsx';
+import './index.css';
+
+import Home from './pages/Home.jsx';
+import Error from './pages/Error.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
