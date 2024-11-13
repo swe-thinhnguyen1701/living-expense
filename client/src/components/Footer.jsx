28954ddc0f05ddbf4599dba34443c7d7c56a6317
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-12 19:47:51 -0600

    build footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,58 @@
-function Footer () {
-    return(
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faAddressCard, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+
+function Footer() {
+    return (
         <footer>
-            <p>© 2023 - Todos os direitos reservados</p>
+            <ul className="list">
+                <li className="list-item">
+                    <Link to="https://www.linkedin.com/in/thinh-nguyen-10b31a218/" target="_blank" className="link">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        <span className="container">
+                            <span className="link-description">
+                                <FontAwesomeIcon icon={faCaretUp} className="caret-up-icon" />
+                                LinkedIn
+                            </span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="list-item">
+                    <Link to="https://github.com/swe-thinhnguyen1701" target="_blank" className="link">
+                        <FontAwesomeIcon icon={faGithub} />
+                        <span className="container">
+                            <span className="link-description">
+                                <FontAwesomeIcon icon={faCaretUp} className="caret-up-icon" />
+                                GitHub
+                            </span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="list-item">
+                    <Link to="mailto:charles.nguyen1701@gmail.com" className="link">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                        <span className="container">
+                            <span className="link-description">
+                                <FontAwesomeIcon icon={faCaretUp} className="caret-up-icon" />
+                                Email
+                            </span>
+                        </span>
+                    </Link>
+                </li>
+                <li className="list-item">
+                    <Link to="https://dashing-rugelach-9db06a.netlify.app/" target="_blank" className="link">
+                        <FontAwesomeIcon icon={faAddressCard} />
+                        <span className="container">
+                            <span className="link-description">
+                                <FontAwesomeIcon icon={faCaretUp} className="caret-up-icon" />
+                                Portfolio
+                            </span>
+                        </span>
+                    </Link>
+                </li>
+            </ul>
+            <p>© 2024 Charles Nguyen</p>
         </footer>
     )
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 function NavBar () {
-
+    
 }
 
 export default NavBar;
