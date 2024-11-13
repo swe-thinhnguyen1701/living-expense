938e7122a2d24d07ae82c99d78d785720a2231f0
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-13 12:57:25 -0600

    build home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,16 @@
-function Home () {
+import logo from "../assets/logo.webp"
+import { Link } from "react-router-dom"
 
+function Home() {
+    return (
+        <article className="home-page">
+            <div>
+                <img src={logo} alt="logo" />
+            </div>
+            <p>Your one-stop solution for managing living expense</p>
+            <Link to="/dashboard" className="link get-started-link">Get Started</Link>
+        </article>
+    );
 }
 
 export default Home;
