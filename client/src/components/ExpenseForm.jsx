6e36c7670e6dec457c348c9c7060725da3273b0e
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-11-14 14:08:57 -0600

    working on expense form

--- client/src/components/ExpenseForm.jsx
+++ client/src/components/ExpenseForm.jsx
@@ -1,14 +1,17 @@
-function ExpenseForm () {
+function ExpenseForm() {
     return (
         <form className="expense-form">
-            <input type="text" placeholder="Description"/>
-            <input type="text" placeholder="Amount"/>
-            <input type="text" placeholder="Category"/>
-            <select name="" id="">
+            <input type="text" placeholder="Description" className="expense-form-input expense-form-input expense-description" />
+            <div className="input-label">
+                <input type="text" placeholder="Amount" className="expense-form-input expense-amount" maxLength={9} />
+                <span className="input-dollar-sign">$</span>
+            </div>
+            <input type="text" placeholder="Category" className="expense-form-input expense-category" />
+            <select name="" id="" className="expense-form-input expense-option">
                 <option value="1">Earning</option>
                 <option value="2">Spending</option>
             </select>
-            <button type="submit">Add</button>
+            <button type="submit" className="btn add-btn">Add</button>
         </form>
     )
 }
