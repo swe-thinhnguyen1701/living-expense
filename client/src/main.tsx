a1d0b78d21d831097eb731667651b51428c9d28f
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-26 00:01:18 -0800

    SETUP: initial set up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
+import theme from "./theme.ts";
+import "./index.css";
+
+import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+]);
+
+
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </StrictMode>,
 )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/graphql": {
+        target: "https://localhost:3000",
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
 })
