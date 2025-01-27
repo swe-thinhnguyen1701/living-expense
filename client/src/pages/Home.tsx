b82855e9d96dce0c1b6b7a01d2a75a776241aaa5
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-27 15:41:45 -0800

    UPDATE: home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import { Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { Box, Heading, Image, Text, VStack } from '@chakra-ui/react';
 import livingExpenseImg from '../assets/living-expense-img.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <VStack justifyContent="center" className="page">
-            <Image src={livingExpenseImg} width={{md: "400px"}} alt="Living Expense image" />
-            <Heading as="h1" fontSize="5xl">Living Expense</Heading>
-            <Text>Take control of your finances with ease. Track your income, expenses, and savings all in one place. Simple, secure, and stress-free.</Text>
+            <Image src={livingExpenseImg} width={{ md: "400px", lg: "550px" }} alt="Living Expense image" className="home-image" />
+            <VStack className="home-content active">
+                <Heading as="h1" fontSize={{ base: "3.815rem", lg: "5.61rem" }} marginBottom={8}>Living Expense</Heading>
+                <Text fontSize={{ md: "1.4rem", xl: "1.47rem" }} textAlign="center">Take control of your finances with ease. Track your income, expenses, and savings all in one place. Simple, secure, and stress-free.</Text>
+                <Link to="/dashboard" className="get-start">Start Tracking Now</Link>
+            </VStack>
         </VStack>
     )
 }
