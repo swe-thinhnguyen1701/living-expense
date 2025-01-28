18a0cfb405bd2fc07eb060e0c0e7d0b29f491cbc
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-27 16:22:34 -0800

    FIX: home page on mobile screen

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { Heading, Flex, Image, Text, VStack } from '@chakra-ui/react';
 import livingExpenseImg from '../assets/living-expense-img.png';
 import { Link } from 'react-router-dom';
 
@@ -9,10 +9,10 @@ const Home = () => {
             <VStack className="home-content active">
                 <Heading as="h1" fontSize={{ base: "3.815rem", lg: "5.61rem" }} marginBottom={8}>Living Expense</Heading>
                 <Text fontSize={{ md: "1.4rem", xl: "1.47rem" }} textAlign="center">Take control of your finances with ease. Track your income, expenses, and savings all in one place. Simple, secure, and stress-free.</Text>
-                <HStack gap={8}>
+                <Flex gap={8} flexDir={{base: "column", md: "row"}} marginTop="50px">
                     <Link to="/dashboard" className="get-start">Start Tracking Now</Link>
                     <Link to="/account" className="get-start">Log in/Sign up</Link>
-                </HStack>
+                </Flex>
             </VStack>
         </VStack>
     )
