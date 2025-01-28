e3aeda0c4d072ffd4b879b71ac5a217d13dc6fdc
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-27 16:10:48 -0800

    BUILD: account page

---
+++ client/src/pages/Account.tsx
@@ -0,0 +1,10 @@
+// This page is under maintainance. Please check back later.
+const Account = () => {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-3xl font-bold">This page is under maintainace. Please check back later</h1>
+        </div>
+    );
+}
+
+export default Account;
