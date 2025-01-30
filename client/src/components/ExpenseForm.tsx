48dad6be4777323715ef3b3e7327658c63b51ead
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-29 20:27:03 -0800

    IMPLEMENT: useExpenses hook for ExpenseForm component

--- client/src/components/ExpenseForm.tsx
+++ client/src/components/ExpenseForm.tsx
@@ -1,25 +1,60 @@
-import { FormControl, Input, InputGroup, InputLeftAddon, Textarea } from "@chakra-ui/react";
+import { Button, Flex, FormControl, FormErrorMessage, Input, InputGroup, InputLeftAddon, Textarea } from "@chakra-ui/react";
+import useExpenses from "../hooks/useExpenses";
 
 const ExpenseForm = () => {
+    const { amountInput, descriptionInput, categoryInput, addExpense, noteRef } = useExpenses();
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        addExpense();
+    };
+
     return (
-        <form>
-            <FormControl className="amount">
-                <InputGroup>
-                    <InputLeftAddon>$</InputLeftAddon>
-                    <Input 
-                    placeholder="Amount"
-                    maxLength={9} />
-                </InputGroup>
-            </FormControl>
+        <form onSubmit={handleSubmit}>
+            <Flex gap={4} marginBottom={5}>
+                <FormControl isInvalid={amountInput.error !== "pass"} className="amount">
+                    <InputGroup>
+                        <InputLeftAddon>$</InputLeftAddon>
+                        <Input
+                            placeholder="Amount"
+                            maxLength={9}
+                            onChange={amountInput.onChange}
+                            value = {amountInput.value} />
+                    </InputGroup>
+                    <FormErrorMessage>
+                        {amountInput.error}
+                    </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={descriptionInput.error !== "pass"}>
+                    <InputGroup>
+                        <Input
+                            placeholder="Description"
+                            maxLength={100}
+                            onChange={descriptionInput.onChange}
+                            value={descriptionInput.value} />
+                    </InputGroup>
+                    <FormErrorMessage>
+                        {descriptionInput.error}
+                    </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={categoryInput.error !== "pass"}>
+                    <InputGroup>
+                        <Input
+                            placeholder="Category"
+                            maxLength={100}
+                            onChange={categoryInput.onChange}
+                            value={categoryInput.value} />
+                    </InputGroup>
+                    <FormErrorMessage>
+                        {categoryInput.error}
+                    </FormErrorMessage>
+                </FormControl>
+            </Flex>
             <InputGroup>
-                <Input placeholder="Description" />
+                <Textarea placeholder="Notes" ref={noteRef}/>
             </InputGroup>
-            <InputGroup>
-                <Input placeholder="Category" />
-            </InputGroup>
-            <InputGroup>
-                <Textarea placeholder="Notes" />
-            </InputGroup>
+            <Button type="submit" bg="blue.500" color="white">
+                ADD
+            </Button>
         </form>
     )
 }
