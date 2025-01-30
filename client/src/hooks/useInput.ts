a12918ca5722b2170ae81aa75334e1834567ec96
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-29 18:57:08 -0800

    BUILD: useInput custom hook

---
+++ client/src/hooks/useInput.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+type Validator = (val: string) => string;
+
+const useInput = (validator: Validator, initialValue = "") => {
+    const [value, setValue] = useState(initialValue);
+    const [error, setError] = useState("pass");
+
+    const validate = () => {
+        const res = validator(value);
+        setError(res);
+        return res;
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newVal = e.target.value;
+
+        const res = validator(newVal);
+
+        // if (res === "pass")
+            setValue(newVal);
+        // else
+            // e.target.value = value;
+
+        setError(res);
+    };
+
+    const reset = () => {
+        setValue("");
+        setError("pass");
+    };
+
+    return {
+        value,
+        setValue,
+        error,
+        onChange,
+        validate,
+        reset
+    }
+}
+
+export default useInput;
